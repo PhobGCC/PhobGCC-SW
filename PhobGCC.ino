ebea9ad0c9090caf8121138fcca85172cd3f61fb
phobo <mephobo@gmail.com>
2021-12-29 12:18:52 -0800

    Messy version of code, just got serialEvent() based communications working, should give reliable communications even with long main loop times. Will try implementing full kalman filter and clean up code. Also added code diagram which is not accurate.

--- PhobGCC.ino
+++ PhobGCC.ino
@@ -23,6 +23,7 @@ using namespace Eigen;
 #define FIT_ADDRESS 0
 
 #define GC_FREQUENCY 1000000
+#define ADC_VAR 1
 
 union Buttons{
 	uint8_t arr[10];
@@ -86,24 +87,34 @@ union Buttons{
 
 unsigned int AStickHX;
 unsigned int AStickHY;
+int writeQueue;
 //double xCoeffs[FIT_ORDER+1] = {0,0,1,0};
 //double yCoeffs[FIT_ORDER+1] = {0,0,1,0};
 //double xCoeffs[FIT_ORDER+1] = {-0.01,-1.41,236};
 //double yCoeffs[FIT_ORDER+1] = {-0.01,3.11,-68.38};
-float fitCoeffs[(FIT_ORDER+1)*2]l;// = {-0.01,-2.2,303.81,0.00,2.31,-61.02};
+float fitCoeffs[(FIT_ORDER+1)*2] = {0,0,0,1,0,  0,0,0,1,0};;
 int calStep;
 unsigned int startBtnSince;
 int lastStartBtn;
 int watchingStart;
 double calPointsX[CALIBRATION_POINTS];
 double calPointsY[CALIBRATION_POINTS];
+unsigned int lastMicros;
+
+VectorXf xState(2);
+VectorXf yState(2);
+MatrixXf xP(2,2);
+MatrixXf yP(2,2);
+float xAccelVar;
+float yAccelVar;
+
 
 static uint8_t probeResponse[CMD_LENGTH_LONG] = {
 	0x08,0x08,0x0F,0xE8,
 	0x08,0x08,0x08,0x08,
 	0x08,0x08,0x08,0xEF,
 	0xFF};
-static uint8_t pollResponse[POLL_LENGTH] = {
+volatile uint8_t pollResponse[POLL_LENGTH] = {
 0x08,0x08,0x08,0x08,
 0x0F,0x08,0x08,0x08,
 0x08,0x08,0xEF,0xEF,
@@ -113,7 +124,7 @@ static uint8_t pollResponse[POLL_LENGTH] = {
 0x08,0x08,0xEF,0xEF,
 0x08,0x08,0xEF,0xEF,
 0xFF};
-static uint8_t originResponse[ORIGIN_LENGTH] = {
+volatile uint8_t originResponse[ORIGIN_LENGTH] = {
 	0x08,0x08,0x08,0x08,
 	0x0F,0x08,0x08,0x08,
 	0x08,0x08,0xEF,0xEF,
@@ -138,8 +149,16 @@ void setup() {
 	lastStartBtn = 0;
 	watchingStart = 0;
 	calStep = -1;
-	EEPROM.get( FIT_ADDRESS, fitCoeffs );
+	//EEPROM.get( FIT_ADDRESS, fitCoeffs );
+	lastMicros = micros();
+	xAccelVar = 1;
+	yAccelVar = 1;
+	writeQueue = 0;
 	
+	xState << 0,0;
+	yState << 0,0;
+	xP << 1000,0,0,100;
+	yP << 1000,0,0,100;
 	
 	analogReadResolution(13);
 	setPinModes();
@@ -161,18 +180,24 @@ void setup() {
 	Serial.println(fitCoeffs[5]);
 	//start hardware serail
 	Serial1.begin(GC_FREQUENCY);
+	//attachInterrupt(0, communicate, FALLING);
 }
 
 void loop() {
-	communicate();
+	//communicate();
 	readButtons();
+	communicate();
 	readSticks();
+	communicate();
 	if(calStep >=0){
 		calibrate();
 	}
 	setPole();
 }
 
+void serialEvent1() {
+	communicate();
+}
 void setPinModes(){
 	pinMode(9,INPUT_PULLUP);
 	pinMode(7,INPUT_PULLUP);
@@ -233,21 +258,32 @@ void readSticks(){
 	
 	//Serial.print("L Trigger");
 	//Serial.println(btn.La,DEC);
-	Serial.print("R Trigger");
-	Serial.println(btn.Ra,DEC);
+	//Serial.print("R Trigger");
+	//Serial.println(btn.Ra,DEC);
 	
 	AStickHX = analogRead(HALX);
 	AStickHY = analogRead(HALY);
 	
+	
 	//btn.Ax = (uint8_t) (xCoeffs[0]*pow(,3) + xCoeffs[1]*pow(AStickHX,2) + xCoeffs[2]*pow(AStickHX,1) + xCoeffs[2]*AStickHX) + 128;
 	//btn.Ay = (uint8_t) (yCoeffs[0]*pow(AStickHY,3) + yCoeffs[1]*pow(AStickHY,2) + yCoeffs[2]*pow(AStickHY,1) + yCoeffs[2]*AStickHY) + 128;
 	
-	btn.Ax = (uint8_t) (fitCoeffs[0]*(AStickHX*AStickHX*AStickHX*AStickHX) + fitCoeffs[1]*(AStickHX*AStickHX*AStickHX) + fitCoeffs[2]*(AStickHX*AStickHX) + fitCoeffs[3]*AStickHX + fitCoeffs[4]);
-	btn.Ay = (uint8_t) (fitCoeffs[5]*(AStickHY*AStickHY*AStickHY*AStickHY) + fitCoeffs[6]*(AStickHY*AStickHY*AStickHY) + fitCoeffs[7]*(AStickHY*AStickHY) + fitCoeffs[8]*AStickHY + fitCoeffs[9]);
+	//btn.Ax = (uint8_t) (fitCoeffs[0]*(AStickHX*AStickHX*AStickHX*AStickHX) + fitCoeffs[1]*(AStickHX*AStickHX*AStickHX) + fitCoeffs[2]*(AStickHX*AStickHX) + fitCoeffs[3]*AStickHX + fitCoeffs[4]);
+	//btn.Ay = (uint8_t) (fitCoeffs[5]*(AStickHY*AStickHY*AStickHY*AStickHY) + fitCoeffs[6]*(AStickHY*AStickHY*AStickHY) + fitCoeffs[7]*(AStickHY*AStickHY) + fitCoeffs[8]*AStickHY + fitCoeffs[9]);
 
 	//btn.Ax = (uint8_t) (fitCoeffs[0]*(AStickHX*AStickHX) + fitCoeffs[1]*AStickHX + fitCoeffs[2]); //+ 128;
 	//btn.Ay = (uint8_t) (fitCoeffs[3]*(AStickHY*AStickHY) + fitCoeffs[4]*AStickHY + fitCoeffs[5]); //+ 128;
+	
+	VectorXf xZ(1);
+	VectorXf yZ(1);
+	xZ <<(fitCoeffs[0]*(AStickHX*AStickHX*AStickHX*AStickHX) + fitCoeffs[1]*(AStickHX*AStickHX*AStickHX) + fitCoeffs[2]*(AStickHX*AStickHX) + fitCoeffs[3]*AStickHX + fitCoeffs[4]);
+	yZ << (fitCoeffs[5]*(AStickHY*AStickHY*AStickHY*AStickHY) + fitCoeffs[6]*(AStickHY*AStickHY*AStickHY) + fitCoeffs[7]*(AStickHY*AStickHY) + fitCoeffs[8]*AStickHY + fitCoeffs[9]);
 
+	runKalman(xZ,yZ);
+	
+	btn.Ax = (uint8_t) xState[0];
+	btn.Ay = (uint8_t) yState[0];
+	
 	btn.Cx = analogRead(17)>>5;//note it looks like these are swapped from what I thought
 	btn.Cy = analogRead(16)>>5;
 	
@@ -296,37 +332,42 @@ void setPole(){
 }
 void communicate(){
 	//Serial.println("communicating");
+	//clear any commands from the last write
+	while(Serial1.available() && (writeQueue > 0)){
+		Serial1.read();
+		writeQueue --;
+	}
 	//check if a command has started (bytes available on the serial line)
 	if (Serial1.available()){
-	//wait for the first 5 bytes of the command to arrive
-	while(Serial1.available()<CMD_LENGTH_SHORT){}
-	//read those 5 bytes
-	for(int i = 0; i <CMD_LENGTH_SHORT; i++){
-	  cmd[i] = Serial1.read();
-	}
+		//wait for the first 5 bytes of the command to arrive
+		while(Serial1.available()<CMD_LENGTH_SHORT){}
+		//read those 5 bytes
+		for(int i = 0; i <CMD_LENGTH_SHORT; i++){
+			cmd[i] = Serial1.read();
+		}
 
-	//parse first 4 bytes of the command
-	for(int i = 0; i <CMD_LENGTH_SHORT-1; i++){
-	  switch(cmd[i]){
-		case 0x08:
-		  cmdByte = (cmdByte<<2);
-		  break;
-		case 0xE8:
-		  cmdByte = (cmdByte<<2)+1;
-		  break;
-		case 0x0F:
-		  cmdByte = (cmdByte<<2)+2;
-		  break;
-		case 0xEF:
-		  cmdByte = (cmdByte<<2)+3;
-		  break;
-		default:
-		  //got garbage data or a stop bit where it shouldn't be
-		  cmdByte = -1;
-	  }
-	}
+		//parse first 4 bytes of the command
+		for(int i = 0; i <CMD_LENGTH_SHORT-1; i++){
+			switch(cmd[i]){
+			case 0x08:
+				cmdByte = (cmdByte<<2);
+				break;
+			case 0xE8:
+				cmdByte = (cmdByte<<2)+1;
+				break;
+			case 0x0F:
+				cmdByte = (cmdByte<<2)+2;
+				break;
+			case 0xEF:
+				cmdByte = (cmdByte<<2)+3;
+				break;
+			default:
+				//got garbage data or a stop bit where it shouldn't be
+				cmdByte = -1;
+			}
+		}
 	//print the command byte over the USB serial connection
-	//Serial.println(cmdByte,HEX);
+	Serial.println(cmdByte,HEX);
 	
 	//decide what to do based on the command
 	switch(cmdByte){
@@ -336,33 +377,34 @@ void communicate(){
 				Serial1.write(pollResponse[i]);
 				//Serial.print(pollResponse[i],HEX);
 		  }
+			writeQueue = POLL_LENGTH;
 		  //Serial.println();
-		  while(Serial1.read() != 0xFF){}
+		  //while(Serial1.read() != 0xFF){}
 		  break;
 		case 0x00:
 		  for(int i = 0; i <CMD_LENGTH_LONG; i++){
 				Serial1.write(probeResponse[i]);
 		  }
-		  while(Serial1.read() != 0xFF){}
+			writeQueue = CMD_LENGTH_LONG;
+		  //while(Serial1.read() != 0xFF){}
 		  break;
 		case 0x41:
 		  for(int i = 0; i <ORIGIN_LENGTH; i++){
 				Serial1.write(originResponse[i]);
 		  }
-		  while(Serial1.read() != 0xFF){}
+			writeQueue = ORIGIN_LENGTH;
+		  //while(Serial1.read() != 0xFF){}
 		  break;
 		default:
 		  //got something strange, try waiting for a stop bit to syncronize
 		  while(Serial1.read() != 0xFF){}
+			writeQueue = 0;
 	  }
 	}
 }
-
-uint8_t calGuideX[CALIBRATION_POINTS] = {128,228,128,198,128,128,128,58,128,28,128,58,128,128,128,198,128};
-uint8_t calGuideY[CALIBRATION_POINTS] = {128,128,128,198,128,228,128,198,128,128,128,58,128,28,128,58,128};
-
 void calibrate(){
-
+	uint8_t calGuideX[CALIBRATION_POINTS] = {128,228,128,198,128,128,128,58,128,28,128,58,128,128,128,198,128};
+	uint8_t calGuideY[CALIBRATION_POINTS] = {128,128,128,198,128,228,128,198,128,128,128,58,128,28,128,58,128};
 	if(calStep >= (CALIBRATION_POINTS)){
 		Serial.println("the x calibration points are:");
 		for(int i = 0; i < CALIBRATION_POINTS; i++){
@@ -485,3 +527,76 @@ void calibrate(){
 	}
 
 }
+void runKalman(VectorXf& xZ,VectorXf& yZ){
+	//Serial.println("Running Kalman");
+	
+	unsigned int thisMicros = micros();
+	unsigned int dT = thisMicros-lastMicros;
+	lastMicros = thisMicros;
+	//Serial.print("the loop time is: ");
+	//Serial.println(dT);
+	
+	MatrixXf Fmat(2,2);
+	//MatrixXf tryF(2);
+	Fmat << 1,dT,
+			 0,1;
+	MatrixXf xQ(2,2);
+	MatrixXf yQ(2,2);
+	xQ << (dT*dT*dT*dT>>2), (dT*dT*dT>>1),
+			 (dT*dT*dT>>1), (dT*dT);
+	yQ = xQ * yAccelVar;
+	xQ = xQ * xAccelVar;
+	
+	MatrixXf sharedH(2,2);
+	sharedH << 1,0,0,0;
+	MatrixXf sharedR(1,1);
+	sharedR << ADC_VAR;
+	dT = micros()-lastMicros;
+	//Serial.println(dT);
+	
+	kPredict(xState,Fmat,xP,xQ);
+	dT = micros()-lastMicros;
+	//Serial.println(dT);
+	
+	kPredict(yState,Fmat,yP,yQ);
+	dT = micros()-lastMicros;
+	//Serial.println(dT);
+	
+	kUpdate(xState,xZ[0],xP,sharedH,sharedR);
+	dT = micros()-lastMicros;
+	//Serial.println(dT);
+	
+	kUpdate(yState,yZ[0],yP,sharedH,sharedR);
+	dT = micros()-lastMicros;
+	//Serial.println(dT);
+}
+void kPredict(VectorXf& X, MatrixXf& F, MatrixXf& P, MatrixXf& Q){
+	//Serial.println("Predicting Kalman");
+	
+	X = F*X;
+	//P = F*P*F.transpose() + Q;
+}
+//void kUpdate(VectorXf& X, VectorXf& Z, MatrixXf& P, MatrixXf& H,  MatrixXf& R){
+void kUpdate(VectorXf& X, float measX, MatrixXf& P, MatrixXf& H,  MatrixXf& R){
+	//Serial.println("Updating Kalman");
+	
+	int sizeState = X.size();
+	//int sizeMeas = Z.size();
+	//MatrixXf A(sizeState,sizeState);
+	//A = P*H.transpose();
+	//MatrixXf B(sizeState,sizeState);
+	//B = H*A+R;
+	MatrixXf K(sizeState,sizeState);
+	//K = A*B.inverse();
+	K = MatrixXf::Identity(sizeState,sizeState);
+	//MatrixXf C(sizeMeas,sizeMeas); 
+	//C = Z - H*X;
+	//X = X + K*(Z - H*X);
+	X = X + K*(measX-X[0]);
+	
+	//MatrixXf D = MatrixXf::Identity(sizeState,sizeState) - K*H;
+	//P = D*P*D.transpose() + K*R*K.transpose();
+}
+
+
+
