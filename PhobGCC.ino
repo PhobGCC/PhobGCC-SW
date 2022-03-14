91082b29a51143627237677109f15f26fae92e47
henriqueop486 <70210443+henriqueop486@users.noreply.github.com>
2022-03-14 00:49:44 -0400

    Prepare to Merge

--- PhobGCC.ino
+++ PhobGCC.ino
@@ -257,7 +257,7 @@ void setup() {
 
 	//start USB serial
 	Serial.begin(57600);
-	Serial.println("Software version 0.17f3 (hopefully Frost remembered to update this message)");
+	//Serial.println("Software version 0.17 (hopefully Phobos remembered to update this message)");
 	Serial.println("This is not a stable version");
 	delay(1000);
 
