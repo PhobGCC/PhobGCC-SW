08fb2e735d4cd6e8ca64f3c939aec746d0c72139
phobo <mephobo@gmail.com>
2022-04-09 12:43:08 -0700

    Removed all hardware files, will be moved to the hardware repo. Updated serial print message for 0.18 tag.

--- PhobGCC.ino
+++ PhobGCC.ino
@@ -290,8 +290,8 @@ void setup() {
 
 	//start USB serial
 	Serial.begin(57600);
-	//Serial.println("Software version 0.17 (hopefully Phobos remembered to update this message)");
-	Serial.println("This is not a stable version");
+	Serial.println("Software version 0.18 for use with 1.0 and 1.1 boards using a teensy 3.2");
+	//Serial.println("This is not a stable version");
 	delay(1000);
 
 	readEEPROM();
