99c776ecc5b4841f418cdc576d6be9915f619aaa
CarVac <c.lo.to.da.down.lo@gmail.com>
2022-06-13 19:46:20 -0400

    mark that it's dev in serial

--- PhobGCC/PhobGCC.ino
+++ PhobGCC/PhobGCC.ino
@@ -17,8 +17,8 @@
 //#include "src/Phob1_1Teensy4_0DiodeShort.h"// For PhobGCC board 1.1 with Teensy 4.0 and the diode shorted
 //#include "src/Phob1_2Teensy4_0.h"          // For PhobGCC board 1.2.x with Teensy 4.0
 
-#define BUILD_RELEASE
-//#define BUILD_DEV
+//#define BUILD_RELEASE
+#define BUILD_DEV
 
 using namespace Eigen;
 
@@ -386,7 +386,7 @@ volatile char _commResponse[_originLength] = {
 void setup() {
     serialSetup();
 		#ifdef BUILD_RELEASE
-    Serial.println("Software version 0.21");
+    Serial.println("Software version 0.22");
     #endif
 		#ifdef BUILD_DEV
     Serial.println("This is not a stable version");
