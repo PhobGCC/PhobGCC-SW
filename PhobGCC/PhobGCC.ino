ecf28e28a08796259e8c8f24f88475ef71822f37
CarVac <c.lo.to.da.down.lo@gmail.com>
2022-06-14 22:36:31 -0400

    comment out board header

--- PhobGCC/PhobGCC.ino
+++ PhobGCC/PhobGCC.ino
@@ -15,7 +15,7 @@
 //#include "src/Phob1_1Teensy3_2.h"          // For PhobGCC board 1.1 with Teensy 3.2
 //#include "src/Phob1_1Teensy4_0.h"          // For PhobGCC board 1.1 with Teensy 4.0 and no diode short
 //#include "src/Phob1_1Teensy4_0DiodeShort.h"// For PhobGCC board 1.1 with Teensy 4.0 and the diode shorted
-#include "src/Phob1_2Teensy4_0.h"          // For PhobGCC board 1.2.x with Teensy 4.0
+//#include "src/Phob1_2Teensy4_0.h"          // For PhobGCC board 1.2.x with Teensy 4.0
 
 //#define BUILD_RELEASE
 #define BUILD_DEV
