546d84388c3714524b1ef736dd69169ac193e68e
phobo <mephobo@gmail.com>
2022-06-27 20:48:49 -0700

    Comment the header file lines

--- PhobGCC/PhobGCC.ino
+++ PhobGCC/PhobGCC.ino
@@ -12,7 +12,7 @@
 
 //Uncomment the appropriate #include line for your hardware by deleting the two slashes at the beginning of the line.
 //#include "src/Phob1_0Teensy3_2.h"          // For PhobGCC board 1.0 with Teensy 3.2
-#include "src/Phob1_0Teensy3_2DiodeShort.h"  // For PhobGCC board 1.0 with Teensy 3.2 and the diode shorted
+//#include "src/Phob1_0Teensy3_2DiodeShort.h"  // For PhobGCC board 1.0 with Teensy 3.2 and the diode shorted
 //#include "src/Phob1_1Teensy3_2.h"          // For PhobGCC board 1.1 with Teensy 3.2
 //#include "src/Phob1_1Teensy4_0.h"          // For PhobGCC board 1.1 with Teensy 4.0 and no diode short
 //#include "src/Phob1_1Teensy4_0DiodeShort.h"// For PhobGCC board 1.1 with Teensy 4.0 and the diode shorted
