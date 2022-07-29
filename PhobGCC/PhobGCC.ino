dc25d31cd3785e87e0862f6ef726ed112ea7907d
CarVac <c.lo.to.da.down.lo@gmail.com>
2022-07-28 21:45:27 -0400

    don't hide version number

--- PhobGCC/PhobGCC.ino
+++ PhobGCC/PhobGCC.ino
@@ -384,9 +384,7 @@ volatile char _commResponse[_originLength] = {
 
 void setup() {
     serialSetup();
-#ifdef BUILD_RELEASE
 	Serial.println("Software version 0.24");
-#endif
 #ifdef BUILD_DEV
 	Serial.println("This is not a stable version");
 #endif
